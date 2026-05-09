b4e64cdc6f2ce6c63dd65c5ba4d54b228bdc29c6
Kuba <lukiukuba@gmail.com>
2026-05-10 01:13:09 +0200

    Achievement added

---
+++ src/main/java/pl/wsb/fitnesstracker/achievement/Achievement.java
@@ -0,0 +1,32 @@
+package pl.wsb.fitnesstracker.achievement;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "achievement")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class Achievement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(name = "earned_at", nullable = false)
+    private LocalDateTime earnedAt;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    public Achievement(String name, LocalDateTime earnedAt, User user) {
+        this.name = name;
+        this.earnedAt = earnedAt;
+        this.user = user;
+    }
+}
